635f854526d1b8d8b7c8219fd619423048066792
Timothy <sanohoang@gmail.com>
2023-04-02 13:28:06 -0500

    completed loops

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    counter = 10
+    while counter > 0:
+        print(counter)
+        counter -= 1
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    int_list = [int ** 2 for int in int_list]
+    return int_list
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    i = 1
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0 and i != 0:
+            i = "FizzBuzz"
+            print(i)
+        elif i % 5 == 0 and i != 0:
+            i = "Buzz"
+            print(i)
+        elif i % 3 == 0 and i != 0:
+            i = "Fizz"
+            print(i)
+        else:
+            print(i)
